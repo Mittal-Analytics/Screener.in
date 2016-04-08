572930122082949907f34cf99ba0294ee6b004eb
Pratyush Mittal <pratyushmittal@gmail.com>
2016-04-08 13:12:06 +0530

    Tests Passing

--- jest/preprocessor.js
+++ jest/preprocessor.js
@@ -1,11 +1,20 @@
 "use strict";
-var babelJest = require('babel-jest');
+
+const babel = require('babel-core');
+const jestPreset = require('babel-preset-jest');
 var webpackAlias = require('jest-webpack-alias');
 
 module.exports = {
-  process: function(src, filename) {
+  process(src, filename) {
     if (filename.indexOf('node_modules') === -1) {
-      src = babelJest.process(src, filename);
+      if (babel.util.canCompile(filename)) {
+        src = babel.transform(src, {
+          auxiliaryCommentBefore: ' istanbul ignore next ',
+          filename,
+          presets: [jestPreset],
+          retainLines: true
+        }).code;
+      }
       src = webpackAlias.process(src, filename);
     }
     return src;
